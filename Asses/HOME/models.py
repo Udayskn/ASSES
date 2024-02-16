4b5e3b7a86570a51de1a30b3f0549cc10540a9cc
Udayskn <2021mcb1253@iitrpr.ac.in>
2024-02-16 23:43:36 +0530

    Home Page
    ->Allowed all hosts
    -> Problem model
    -> problem_model at admin site
    ->HomeView
    ->Home.html
    -> added home urls
    
    ***migrations must be done to update database

--- Asses/HOME/models.py
+++ Asses/HOME/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Problem(models.Model):
+    name=models.CharField(max_length=40)
+    description=models.TextField()
+    diff_level=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
--- Asses/HOME/views.py
+++ Asses/HOME/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from HOME.models import Problem
+from django.views import generic
 # Create your views here.
+class HomeView(generic.ListView):
+    template_name = 'HOME/home.html'
+    context_object_name = 'Problem_list'
+
+    def get_queryset(self):
+        return Problem.objects.all()
