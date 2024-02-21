6b29a5b7563827c37d95c2b967fb404bfc77b49c
Udayskn <2021mcb1253@iitrpr.ac.in>
2024-02-21 18:21:11 +0530

    PROBLEM_PAGE
    added view, html page taken data from problem model, connected with homepage

--- Asses/HOME/urls.py
+++ Asses/HOME/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 app_name='HOME'
 urlpatterns = [
-    path('',views.HomeView.as_view(),name='home')
-    
+    path('',views.HomeView.as_view(),name='home'),
+    path('<int:problem_id>',views.ProblemView,name='Problem'),
 ]
--- Asses/HOME/views.py
+++ Asses/HOME/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
 from HOME.models import Problem
 from django.views import generic
+from django.shortcuts import render, redirect, get_object_or_404
+from HOME.forms import CodeForm
+from django.conf import settings
+
 # Create your views here.
 class HomeView(generic.ListView):
     template_name = 'HOME/home.html'
@@ -8,3 +11,14 @@ class HomeView(generic.ListView):
 
     def get_queryset(self):
         return Problem.objects.all()
+def ProblemView(request, problem_id):
+    #template_name='HOME/problem.html'
+    problem = get_object_or_404(Problem, id=problem_id)
+    #form = CodeForm()
+    context = {
+        'ProblemName': problem.name,
+        'Problemstatement': problem.description,
+        'problem_id': id,
+        'problem': problem,
+    }
+    return render(request, 'HOME\problem.html', context)
