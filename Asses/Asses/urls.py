eac3888cc9ef1bf0d04a6214b601d11801aed5d5
Udayskn <2021mcb1253@iitrpr.ac.in>
2024-03-04 10:53:28 +0530

    improve

--- Asses/Asses/urls.py
+++ Asses/Asses/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('HOME/',include('HOME.urls')),
-    path('AUTHENTICATION/',include('AUTHENTICATION.urls'))
+    path('AUTHENTICATION/',include('AUTHENTICATION.urls')),
+    # path('', TemplateView.as_view(template_name='Asses/about.html'), name='about'),
     # path('RESGITER/',include('RESGITER.urls'))
 ]
